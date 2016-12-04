f43ded720eca33a0e22e58f6f5699247e441432b
Omphalus Kua <haluskua.one@gmail.com>
2016-12-04 16:40:00 +1300

    styling the header with flex properties

--- header.php
+++ header.php
@@ -25,39 +25,41 @@
 
 	<header id="masthead" class="site-header" role="banner">
 
-	<div id="header-3section">
-		<div id="secion1">
-			<p>empty for now</p>
-		</div>
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<?php else : ?>
-						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-					<?php
-					endif;
+		<div id="header-3section">
+			<div id="secion1">
+				<p>empty for now</p>
+			</div>
+			<div class="site-branding">
+				<?php
+				if ( is_front_page() && is_home() ) : ?>
+							<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+						<?php else : ?>
+							<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+						<?php
+						endif;
 
-					$description = get_bloginfo( 'description', 'display' );
-					if ( $description || is_customize_preview() ) : ?>
-						<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-					<?php
-					endif; ?>
-				</div>
-					<div id="section3">
-						<ul class="social-medias">
-							<li><a href="#">facebook</a></li>
-							<li><a href="#">twitter</a></li>
-							<li><a href="#">pinterest</a></li>
-						</ul>
-					</div>
+						$description = get_bloginfo( 'description', 'display' );
+						if ( $description || is_customize_preview() ) : ?>
+							<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+						<?php
+						endif; ?>
+			</div>
+
+			<div id="section3">
+				<ul class="social-medias">
+					<li><a href="#">facebook</a></li>
+					<li><a href="#">twitter</a></li>
+					<li><a href="#">pinterest</a></li>
+				</ul>
+			</div>
+		
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'flexsass' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
-		
+			
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
