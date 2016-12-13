7bf9e1adf1fcc241d2fe78134d84c8508e1fee41
Omphalus Kua <haluskua.one@gmail.com>
2016-12-14 01:56:37 +1300

    worked on the menu navigation layout

--- header.php
+++ header.php
@@ -52,7 +52,6 @@
 			</div>
 		</div> <!-- .rowcenterdiv -->
 
-	
 					<?php
 					if ( is_front_page() && is_home() ) : ?>
 					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1><p></p>
